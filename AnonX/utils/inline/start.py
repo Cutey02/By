4ef2259bac6fa8cf0ec054b4450f9ad9dc70ed7e
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-07-02 19:45:46 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,7 +9,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⎆ ⌜ᴀᴅᴅ ᴍᴇ ᴛᴏ ɢʀᴏᴜᴘ⌟",
+                text="⎘ ᴀᴅᴅ ᴍᴇ ᴛᴏ ɢʀᴏᴜᴘ ➕",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
