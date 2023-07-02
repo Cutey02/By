8b3accdf3d018d8d207f2f8dd74da17f331a405f
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-07-02 17:50:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,18 +76,18 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/c421e792965559b846842.jpg")
+START_IMG_URL = getenv("START_IMG_URL", None)
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://graph.org/file/c421e792965559b846842.jpg",
+    None,
 )
 
 PLAYLIST_IMG_URL = "https://graph.org/file/35c464bd07d8613092611.jpg"
 
-GLOBAL_IMG_URL = "https://graph.org/file/c421e792965559b846842.jpg"
+GLOBAL_IMG_URL = None
 
-STATS_IMG_URL = "https://graph.org/file/c421e792965559b846842.jpg"
+STATS_IMG_URL = "https://graph.org/file/35c464bd07d8613092611.jpg"
 
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 
